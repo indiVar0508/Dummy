5147458f2017accacbd08fe7b20a25382c76d3f3
Indivar Mishra <indimishra@gmail.com>
2022-11-09 12:58:41 +0530

    Changes

--- dummy/main.py
+++ dummy/main.py
@@ -20,3 +20,19 @@ def greet(name):
     val = random.random()
     print(f"Hi, {name}" if val < 0.5 else f"Hola, {name}" if (val > 0.5) and (val < 0.8) else f"Bojour, {name}")
 
+
+def depart(name):
+    """The method Deprats you with your name with random probability
+
+    Examples:
+        >>> depart("Indi")
+        Bye, Indi
+
+    Args:
+        name: Your name
+
+    Returns:
+        Greeting message
+    """
+    val = random.random()
+    print(f"Bye, {name}" if val < 0.5 else f"adiós, {name}" if (val > 0.5) and (val < 0.8) else f"au revoir, {name}")
