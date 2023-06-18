946b8867d27b5f7e1c0b5f5e92a021c271e91d92
Indivar Mishra <indimishra@gmail.com>
2023-06-18 12:46:59 +0530

    chore1

--- indi_dummy/main.py
+++ indi_dummy/main.py
@@ -18,7 +18,7 @@ def greet(name):
         Greeting message
     """
     chance = random.random()
-    print(f"Hi, {name}" if chance < 0.5 else f"Hola, {name}" if (chance > 0.5) and (chance < 0.8) else f"Bojour, {name}")
+    print(f"Hi, {name}" if chance < 0.6 else f"Hola, {name}" if (chance > 0.5) and (chance < 0.8) else f"Bojour, {name}")
 
 
 def depart(name):
